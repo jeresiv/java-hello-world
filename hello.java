17341ed46dd2ef6a1e917199cce50721339861bc
Mitchell Derry <derr0037@algonquinlive.com>
2024-05-07 20:01:59 -0400

    Added name and student number

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello CST8132!"); 
+        System.out.println("Mitchell Derry - 041027049"); 
     }
 }
