61dc25da2527528f2fc7a1fd30c8a6b49b113cb6
Samarveer Singh Toor <toor0031@algonquinlive.com>
2024-05-08 13:14:55 -0400

    changed to Samarveer Singh Toor: 041130378

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Professor!"); 
+        System.out.println("Samarveer Singh Toor: 041130378"); 
     }
 }
