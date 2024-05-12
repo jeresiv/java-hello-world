2db1b099a1cd73e261df8dd4c5acb9ce7de7d36e
rwamE <rwam0003@algonquinlive.com>
2024-05-11 20:17:50 -0400

    fixed file

--- hello.java
+++ hello.java
@@ -1,9 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello World"); 
-=======
+
         System.out.println("Hello Students! Please fix a merge conflict for a bonus mark"); 
->>>>>>> upstream/main
+
     }
 }
